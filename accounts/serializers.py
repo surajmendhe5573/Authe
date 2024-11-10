9fb002cb879d380ac214c332887dbba771a37016
surajmendhe5573 <surajmendhe470@gmail.com>
2024-11-10 11:20:59 +0530

    changes

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     password2= serializers.CharField(write_only= True)
     class Meta:
         model= User
